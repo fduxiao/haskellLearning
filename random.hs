809e22c7f686af6316e2aaaca1387672a6754b4e
xiao <xiao@localhost>
2016-03-29 15:07:24 +0800

    random

---
+++ random.hs
@@ -0,0 +1,26 @@
+import System.Random
+
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+    let (firstCoin, newGen) = random gen
+        (secondCoin, newGen') = random newGen
+        -- (thirdCoin, newGen'') = random newGen'
+        (thirdCoin, _) = random newGen'
+    in  (firstCoin, secondCoin, thirdCoin)
+
+randoms' :: (RandomGen g, Random a) => g -> [a]
+randoms' gen = let (value, newGen) = random gen in value:randoms' newGen
+
+finiteRandoms :: (RandomGen g, Random a, Num n, Eq n) => n -> g -> ([a], g)
+finiteRandoms 0 gen = ([], gen)
+finiteRandoms n gen =
+    let (value, newGen) = random gen
+        (restOfList, finalGen) = finiteRandoms (n-1) newGen
+    in  (value:restOfList, finalGen)
+
+main :: IO ()
+main = do
+  gen <- getStdGen
+  putStr $ take 20 (randomRs ('a','z') gen)
+  print . show $ threeCoins gen;
+  return ()
